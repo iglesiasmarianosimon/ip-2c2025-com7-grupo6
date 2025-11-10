24a10d788ba1acbcd20f8067ca5e0041f72bb7d8
Mariano Iglesias <iglesias.marianosimon@gmail.com>
2025-11-10 01:53:26 -0300

    10112025-0055

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -13,11 +13,34 @@ def init(vals):
     j = 0
 
 def step():
-    # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+    global items, n, i, j
+
+    if i >= n - 1:
+        return {"done": True}
+    a = j
+    b = j + 1
+    swap = False
+
+    if items[a] > items[b]:
+        items[a], items[b] = items[b], items[a]
+        swap = True
+
+    j += 1
+    if j >= n - i - 1:
+        j = 0
+        i += 1
+    return {"a": a, "b": b, "swap": swap, "done": False}
+
+"""
+En pseudocodigo:
+Tengo una lista [5,6,4,2,1]
+La lista no se repite
+un número es mayor que otro.
+
+Tengo que lograr comparar dos números y si el de la derecha es menor, ubicarlo en la posición del número que comparo inicialmente.
+Es decir, para (for) una lista de números (in range lista) Si el primer número es mayor comparado con el siguiente número de la lista  (posición > posición +1) entonces cambiar de posición
+Entonces buscamos el mayor número de la lista (menos uno porque no hay uno mayor) y dentro de esa búsqueda comparar si son mayores.
+
+"""
+
+
