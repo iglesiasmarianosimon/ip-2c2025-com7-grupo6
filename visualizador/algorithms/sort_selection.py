95e277bfa3b3288441c9e4841c776c8fa8523db3
LeannBenitez <leannbenitez04@gmail.com>
2025-11-11 20:32:09 -0300

    Update sort_selection.py

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -7,8 +7,13 @@ j = 0          # cursor que recorre y busca el mínimo
 min_idx = 0    # índice del mínimo de la pasada actual
 fase = "buscar"  # "buscar" | "swap"
 
+#METRICAS
+vecesQueCompara=0
+vecesQueCambia=0
+
+
 def init(vals):
-    global items, n, i, j, min_idx, fase
+    global items, n, i, j, min_idx, fase, vecesQueCompara, vecesQueCambia
     items = list(vals)
     n = len(items)
     i = 0
@@ -19,13 +24,15 @@ def init(vals):
 def step():
     global items, n, i, j, min_idx, fase
     if i>=n-1: 
+        print("Selection comparo:", vecesQueCompara, "y cambio", vecesQueCambia) #Luego de ordenar todo mostramos cuantas veces comparo y cambio
         return {"done": True}
 #Si ya esta todo ordenado, devolvemos "done", True para terminar el algoritmo
-
+#Para ver el resultado de las metricas vamos al visualizador y pulsamos F12, alli se abrira una pequeña pestaña, donde nos aparecera una opcion que dice "Console", alli podemos ver los resultados
     
 #Cambiamos a fase "buscar", reccoremos toda la lista para ver que numero es el mas pequeño
     if fase== "buscar":
         if j<n:
+            vecesQueCompara=VecesQueCompara+1 #Le agregamos uno cada vez que este compara dos numeros
             if items[j]<items[min_idx]:
                 min_idx=j
             j=j+1
@@ -39,6 +46,7 @@ def step():
         if min_idx !=i:
             items[i], items[min_idx] = items[min_idx], items[i]
             swap=True
+            vecesQueCambia=vecesQueCambia+1 #Sumamos uno cada vez que dos numeros cambian de lugar
 
 #Después de cambiar los valores, el algoritmo se prepara para seguir con el próximo movimiento (si es que todavia queda por ordenar), en simples palabras prepara todo para seguir con el siguiente valor  
         a=i
